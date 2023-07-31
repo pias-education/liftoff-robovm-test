fe54feed1146560f3e679195c8e2db3a4bd7cbba
Maximilian Wende <dasisdormax@mailbox.org>
2023-07-31 09:17:28 +0200

    Initial Commit (modified liftoff project)

---
+++ ios/src/main/java/com/pias_education/ios/IOSLauncher.java
@@ -0,0 +1,23 @@
+package com.pias_education.ios;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.pias_education.LiftOff;
+
+/** Launches the iOS (RoboVM) application. */
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration configuration = new IOSApplicationConfiguration();
+        return new IOSApplication(new LiftOff(), configuration);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
